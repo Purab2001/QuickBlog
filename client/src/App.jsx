22504f35c96a4992b70e88187454974fc76173ff
Abir Shahadat Purab <a.s.purab0@gmail.com>
2025-06-12 12:35:44 +0600

    Updated router, improve Blog comments/loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 
